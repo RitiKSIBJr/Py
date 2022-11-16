1779c0793d74f302431b6d6e6bbeba717e086eca
RitiKSiBJr <letter.ritik060@gmail.com>
2022-11-16 20:26:20 +0545

    unittest

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Unittest',
+    version='0.0.0',
+    description='Unittest',
+    author='Ritik',
+    author_email='letter.ritik060@gmail.com',
+    url='https://www.github.com/RitikSibjr/Py',
+    packages=['first']
+)
