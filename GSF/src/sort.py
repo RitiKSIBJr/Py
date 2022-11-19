10121a225559a9d461539fb1fae19fc258d3f68d
RitiKSiBJr <letter.ritik060@gmail.com>
2022-11-19 16:15:52 +0545

    Analyze

---
+++ GSF/src/sort.py
@@ -0,0 +1,32 @@
+import csv
+from collections import defaultdict
+
+def most_win_till_1916():
+    with open('goalscorers.csv','r', encoding='utf-8') as file:
+        reader = csv.DictReader(file, fieldnames=['date','away_team','home_team', 'team', 'scorer', 'minute', 'own_goal', 'penalty'])
+        stack = defaultdict(int)
+
+        for line in reader:
+            if line['date'].split("-")[0] <= '1916':
+                stack[line['team']] += 1
+
+        for x in stack:
+            print(x+'-'+str(stack[x]))
+
+
+def goal_scored_in_a_year():
+    with open('goalscorers.csv','r', encoding='utf-8') as file:
+        reader = csv.DictReader(file, fieldnames=['date','away_team','home_team', 'team', 'scorer', 'minute', 'own_goal', 'penalty'])
+        scorer = defaultdict(int)
+
+        for line in reader:
+            scorer[line['scorer'], line['date'].split("-")[0]] += 1
+            
+
+        for x in scorer:
+            print(str(x)+' - '+str(scorer[x]))
+                
+
+if __name__ == "__main__":
+    most_win_till_1916()
+    goal_scored_in_a_year()
